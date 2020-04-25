8c4718275a75e6ba45bf679ce7c4928ae0402296
Long Nguyen <n.h.long.9697@gmail.com>
2020-04-25 22:54:10 +0700

    tivi app

---
+++ bootcampprep/week3/13.Array-2/exercise/tic-tac-toe/script.js
@@ -0,0 +1,37 @@
+const statusDisplay = document.querySelector(".game--status");
+let gameActive = true;
+
+let currentPlayer = "X";
+
+let gameState = ["", "", "", "", "", "", "", "", ""];
+
+const winningMessage = () => `Player ${currentPlayer} has won!`;
+const drawMessage = () => `Game ended in a draw!`;
+const currentPlayerTurn = () => `It's ${currentPlayer}'s turn`;
+
+statusDisplay.innerHTML = currentPlayerTurn();
+function handleCellPlayed() {
+  const clickedCell = clickedCellEvent.target;
+
+  const clickedCellIndex = parseInt(
+    clickedCell.getAttribute("data-cell-index")
+  );
+
+  if (gameState[clickedCellIndex] !== "" || !gameActive) {
+    return;
+  }
+
+  handleCellPlayed(clickedCell, clickedCellIndex);
+  handleResultValidation();
+}
+
+function handlePlayerChange() {}
+function handleResultValidation() {}
+function handleCellClick() {}
+function handleRestartGame() {}
+document
+  .querySelectorAll(".cell")
+  .forEach((cell) => cell.addEventListener("click", handleCellClick));
+document
+  .querySelector(".game--restart")
+  .addEventListener("click", handleRestartGame);
