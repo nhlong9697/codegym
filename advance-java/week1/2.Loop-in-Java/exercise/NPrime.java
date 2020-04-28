a8dacd602f824863b587de75cdb21193a82bc39e
Long Nguyen <n.h.long.9697@gmail.com>
2020-04-28 09:55:14 +0700

    fix bug prime practice & add prime exercise

---
+++ advance-java/week1/2.Loop-in-Java/exercise/NPrime.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class NPrime {
+  public static boolean isPrime(int number) {
+    if (number < 2) {
+      return false;
+    } else {
+      int i = 2;
+      while (i <= Math.sqrt(number)) {
+        if (number % i == 0) {
+          return false;
+        }
+        i++;
+      }
+    }
+    return true;
+  }
+
+  public static void main(String[] args) {
+    Scanner input = new Scanner(System.in);
+    System.out.println("Enter number of prime number to display: ");
+    int n = input.nextInt();
+    int count = 0;
+    int potentialPrime = 2;
+    while (count < n) {
+      if (isPrime(potentialPrime)) {
+        System.out.printf("%d ", potentialPrime);
+        count += 1;
+      }
+      potentialPrime += 1;
+    }
+  }
+}
