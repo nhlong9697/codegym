6265dbd7321ac9570e4eb64059690c9bf727543c
Long Nguyen <n.h.long.9697@gmail.com>
2020-05-26 15:25:21 +0700

    validate phone and class name

---
+++ advance-java/week4/19.String-and-Regex/exercise/validate-class-name/src/ClassNameExampleTest.java
@@ -0,0 +1,20 @@
+public class ClassNameExampleTest {
+  private static ClassNameExample classNameExample;
+
+  public static final String[] validClassNames = new String[] {"C0318G","A1299M","C0320H"};
+  public static final String[] invalidClassNames = new String[] {"M132g","M0318G","P0323A","adfasdf","12314","C2120H"};
+
+  public static void main(String[] args) {
+    classNameExample = new ClassNameExample();
+
+    for (String className : validClassNames) {
+      boolean isValid = classNameExample.validate(className);
+      System.out.println("Class name " + className + " is valid: " + isValid);
+    }
+
+    for (String className : invalidClassNames) {
+      boolean isValid = classNameExample.validate(className);
+      System.out.println("Class name " + className + " is valid: " + isValid);
+    }
+  }
+}
