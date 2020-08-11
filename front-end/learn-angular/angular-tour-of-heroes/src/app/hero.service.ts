922f061db8b5a444ee674b36c40c56e70444a986
Long Nguyen <n.h.long.9697@gmail.com>
2020-08-11 13:29:29 +0700

    crud

--- front-end/learn-angular/angular-tour-of-heroes/src/app/hero.service.ts
+++ front-end/learn-angular/angular-tour-of-heroes/src/app/hero.service.ts
@@ -16,6 +16,9 @@ import { catchError, map, tap } from 'rxjs/operators';
 })
 export class HeroService {
   private heroesUrl = 'api/heroes';
+  httpOptions = {
+    headers: new HttpHeaders({ 'Content-type': 'application/json' }),
+  };
 
   constructor(
     private messageService: MessageService,
@@ -42,17 +45,48 @@ export class HeroService {
     );
   }
 
+  //handle http operation that failed
+  //let the app continue
+  //@param operation - name of the operation that failed
+  //@param result optional value to return as default
   private handleError<T>(operation = 'operation', result?: T) {
     return (error: any): Observable<T> => {
+      //TODO: send the errorr to remote logging infrastructure
       console.log(error);
 
+      //TODO: better way of return to user;
       this.log(`${operation} failed: ${error.message}`);
 
+      //TODO: keep app running by returning an empty result
       return of(result as T);
     };
   }
 
   private log(message: String) {
     this.messageService.add(`HeroService: ${message}`);
+  }
+
+  updateHero(hero: Hero): Observable<any> {
+    return this.http.put(this.heroesUrl, hero, this.httpOptions).pipe(
+      tap((_) => this.log(`update hero id=${hero.id}`)),
+      catchError(this.handleError<any>('updateHero'))
+    );
+  }
+
+  addHero(hero: Hero): Observable<Hero> {
+    return this.http.post<Hero>(this.heroesUrl, hero, this.httpOptions).pipe(
+      tap((newHero: Hero) => this.log(`added Hero with id=${newHero.id}`)),
+      catchError(this.handleError<Hero>('addHero'))
+    );
+  }
+
+  deleteHero(hero: Hero | number): Observable<Hero> {
+    const id = typeof hero === 'number' ? hero : hero.id;
+    const url = `${this.heroesUrl}/${id}`;
+
+    return this.http.delete<Hero>(url, this.httpOptions).pipe(
+      tap((_) => this.log(`delete hero id=${id}`)),
+      catchError(this.handleError<Hero>('deleteHero'))
+    );
   }
 }
