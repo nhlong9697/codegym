94eb9f14c0489b1c492826415fa213ce2ef6aaba
Long Nguyen <n.h.long.9697@gmail.com>
2020-08-11 14:50:00 +0700

    finish search

--- front-end/learn-angular/angular-tour-of-heroes/src/app/app.module.ts
+++ front-end/learn-angular/angular-tour-of-heroes/src/app/app.module.ts
@@ -13,6 +13,7 @@ import { HttpClientModule } from '@angular/common/http';
 
 import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
 import { InMemoryDataService } from './in-memory-data.service';
+import { HeroSearchComponent } from './hero-search/hero-search.component';
 @NgModule({
   declarations: [
     AppComponent,
@@ -20,6 +21,7 @@ import { InMemoryDataService } from './in-memory-data.service';
     HeroDetailComponent,
     MessagesComponent,
     DashboardComponent,
+    HeroSearchComponent,
   ],
   imports: [
     BrowserModule,
